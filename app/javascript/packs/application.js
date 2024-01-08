5717f06b4394fbe2be66a7d3bf2d9d03e6e82718
EC2 Default User <ec2-user@ip-172-31-30-127.ap-northeast-1.compute.internal>
2024-01-08 14:12:13 +0000

    header編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "jquery";
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../stylesheets/application.scss';
+import Swiper from 'swiper/swiper-bundle.js';  //この行を追記
+import 'swiper/swiper-bundle.css';
+//この行を追記
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
