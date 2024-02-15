77c8a8c24a3b28d79bab36b903eeb7f5b46b8928
EC2 Default User <ec2-user@ip-172-31-30-127.ap-northeast-1.compute.internal>
2024-02-15 12:48:12 +0000

    不要なコード削除

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,25 +1,8 @@
 class Public::NotificationsController < ApplicationController
   before_action :authenticate_user!
 
-  # def index
-  #   @notifications = current_user.passive_notifications.where(checked: false).order(created_at: :desc).page(params[:page]).per(6)
-  # end
-
-  # def mark_as_read
-  #   current_user.passive_notifications.where(checked: false).update_all(checked: true)
-  #   redirect_to request.referer, notice: "すべて既読にしました"
-  # end
-
-  # def update
-  #   notification = Notification.find(params[:id])
-  #   notification.update(checked: true)
-  #   redirect_to request.referer
-  # end
-
   def index
-    notices = current_user.passive_notifications # .order(created_at: :desc)
-    # @unchecked_notifications = @notices.where(is_checked: false).order(created_at: :desc)
-
+    notices = current_user.passive_notifications 
     case params[:option]
     when 'uncheck'
       # 未読
@@ -43,11 +26,6 @@ class Public::NotificationsController < ApplicationController
       # latest, oldest以外の文字列の場合
       @notifications = notices.order(created_at: :desc)
     end
-
-
-    # 通知を確認済みに更新
-    # current_user.passive_notifications.update_all(is_checked: true)
-    # render "public/notifications/index"
   end
 
   def checked
